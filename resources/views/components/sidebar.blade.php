4d431b0b948d9912fff2deb2cf8af913d822a35f
t3riteki <104114992+t3riteki@users.noreply.github.com>
2025-12-07 19:31:29 +0800

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -40,7 +40,7 @@
                     </a>
 
                     <a href="/modules" class="flex items-center gap-2 px-4 py-2 rounded-lg hover:bg-red-90 hover:text-red-800 transition
-                        {{ request()->is('cmodules*') ? 'text-red-800 bg-red-100' : 'text-gray-700 bg-white' }}">
+                        {{ request()->is('modules*') ? 'text-red-800 bg-red-100' : 'text-gray-700 bg-white' }}">
                         <i class="ri-book-2-line text-lg"></i> Modules
                     </a>
                 </div>
